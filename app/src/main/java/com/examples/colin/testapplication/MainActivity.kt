1465304ba33ec19363bf0625cecc7cace87e2c0b
wjuz84 <wjuz84@gmail.com>
2020-10-26 21:55:16 +0800

    Updated with navigation and message capture

--- app/src/main/java/com/examples/colin/testapplication/MainActivity.kt
+++ app/src/main/java/com/examples/colin/testapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.examples.colin.testapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -16,6 +17,14 @@ class MainActivity : AppCompatActivity() {
     //code
         Log.i("MainActivity", "Button was clicked !")
         Toast.makeText(this, "Button was clicked!", Toast.LENGTH_SHORT).show()
+        }
+
+        btnSendMsgToNextActivity.setOnClickListener {
+            val message: String = etUserMessage.text.toString()
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
         }
 
     }
---
+++ app/src/main/java/com/examples/colin/testapplication/SecondActivity.kt
@@ -0,0 +1,13 @@
+package com.examples.colin.testapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+    }
+}
